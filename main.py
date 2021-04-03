ffeacf707cd531987772274fcb5552aa20785f15
Roshan Kaleem <rkaleem.bscs18seecs@seecs.edu.pk>
2021-04-03 14:27:45 -0700

    commented

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import cv2
 from collections import Counter
 
 
-def RGB2HEX(color):
+def RGB2HEX(color):  # convert RGB codes to hex
     return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
 
-def get_image(image_path):
+
+def get_image(image_path):  # convert image back to RGB
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
-def get_colors(image,numberofcolors,showchart):
+
+def get_colors(image, numberofcolors, showchart):
     modified_image = cv2.resize(image, (600, 400), interpolation=cv2.INTER_AREA)
     modified_image = modified_image.reshape(modified_image.shape[0] * modified_image.shape[1], 3)
     clf = KMeans(n_clusters=numberofcolors)
